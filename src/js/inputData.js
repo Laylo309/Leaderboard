41f1dc00355a65dce340006c604866e57798c876
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-02 06:12:10 +0500

    Fix linter errors

--- src/js/inputData.js
+++ src/js/inputData.js
@@ -6,7 +6,7 @@ const scoreTable = document.getElementById('leaderboard-table');
 const inputData = () => {
   scoreTable.innerHTML = 'Score';
   allData.forEach((data) => {
-	scoreTable.innerHTML += `<tr><td>${data.Name}: ${data.Score}</td></tr>`;
+    scoreTable.innerHTML += `<tr><td>${data.Name}: ${data.Score}</td></tr>`;
   });
 };
 

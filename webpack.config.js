8744b4376364983a609cbf27293ef5f3721640b2
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-02 11:00:36 +0500

    Make visible dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -11,11 +10,6 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   module: {
     rules: [
       {

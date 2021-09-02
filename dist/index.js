f697e17144a1456f66076b601481c83fe4bf328e
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-02 19:30:13 +0500

    Make required changes

---
+++ dist/index.js
@@ -0,0 +1,106 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_inputData_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/inputData.js */ \"./src/js/inputData.js\");\n\n\n(0,_js_inputData_js__WEBPACK_IMPORTED_MODULE_0__.inputData)();\n\n\n//# sourceURL=webpack://leaderboard/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/js/allData.js":
+/*!***************************!*\
+  !*** ./src/js/allData.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"allData\": () => (/* binding */ allData)\n/* harmony export */ });\nconst allData = [\n  {\n    Name: 'Laylo',\n    Score: 50,\n  }, {\n    Name: 'Laylo',\n    Score: 25,\n  }, {\n    Name: 'Laylo',\n    Score: 5,\n  }, {\n    Name: 'Laylo',\n    Score: 51235,\n  }, {\n    Name: 'Laylo',\n    Score: 2550,\n  }, {\n    Name: 'Laylo',\n    Score: 540,\n  }, {\n    Name: 'Laylo',\n    Score: 70,\n  }, {\n    Name: 'Laylo',\n    Score: 80,\n  },\n];\n\n// eslint-disable-next-line import/prefer-default-export\n\n\n//# sourceURL=webpack://leaderboard/./src/js/allData.js?");
+
+/***/ }),
+
+/***/ "./src/js/inputData.js":
+/*!*****************************!*\
+  !*** ./src/js/inputData.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"inputData\": () => (/* binding */ inputData)\n/* harmony export */ });\n/* harmony import */ var _allData__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./allData */ \"./src/js/allData.js\");\n/* eslint-disable import/extensions */\n\n\nconst scoreTable = document.getElementById('leaderboard-table');\n\nconst inputData = () => {\n  scoreTable.innerHTML = 'Score';\n  _allData__WEBPACK_IMPORTED_MODULE_0__.allData.forEach((data) => {\n    scoreTable.innerHTML += `<tr><td>${data.Name}: ${data.Score}</td></tr>`;\n  });\n};\n\n// eslint-disable-next-line import/prefer-default-export\n\n\n//# sourceURL=webpack://leaderboard/./src/js/inputData.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -10,6 +11,14 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {

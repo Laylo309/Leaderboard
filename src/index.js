fa441e5f4c4a082f305a20a21e24596186dc3aed
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2022-02-19 05:50:58 +0500

    Add new api endpoint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import _ from 'lodash'; // eslint-disable-line
 import displayScore from './js/inputData.js';
 import { postScore, getByApi } from './js/postScores.js';
 
-const myApi = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N08D8RBmZ6I1HYiYFCDT/scores';
+// const myApi = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N08D8RBmZ6I1HYiYFCDT/scores';
+const myApi = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/itAQRCB8H1z21PFkumFt/scores';
 const scoresForm = document.getElementById('scoresForm');
 const refreshBttn = document.getElementById('refreshBttn');
 
